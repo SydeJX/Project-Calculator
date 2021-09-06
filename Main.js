3649ed07f13efdec35056673bcc12ba0c69fc74c
SydeJX <sydeweiz@gmail.com>
2021-09-06 14:16:11 +0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,7 +1,22 @@
 var display = document.getElementById('display');
+var slash = document.getElementById('Slash');
+var procent = document.getElementById('Procent');
+var seven = document.getElementById('Seven');
+var eight = document.getElementById('Eight');
+var nine = document.getElementById('Nine');
+var four = document.getElementById('Four');
+var five = document.getElementById('five');
+var six = document.getElementById('six');
+var minus = document.getElementById('Minus');
+var one = document.getElementById('One');
+var two = document.getElementById('Two');
+var three = document.getElementById('Three');
+var plus = document.getElementById('Plus');
+var zero = document.getElementById('Zero');
+var point = document.getElementById('point');
+var equel = document.getElementById('Equel');
 
-function MyFunction() {
-if (display.value.length = 3){
-  display.value=eval;
-}
+function Onclick(){
+if(slash.onlick)
+display.value =+ '/';
 }
